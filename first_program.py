a021b15027483648c049268bdf788b02862f68ba
Digvijay-singh420 <60250432+Digvijay-singh420@users.noreply.github.com>
2020-01-24 11:51:16 +0530

    worng programe

---
+++ first_program.py
@@ -0,0 +1,11 @@
+a=float(input('enter the number'))
+r=5
+h=10
+f=15
+v=3.14*r*r*h
+v
+         print('number is negative')
+elif a==0:
+         print('number is zero')
+else:
+        print('number is positive')
